b62ce2b1efa50a929ea7146f103ce0e541d8fd21
Marina Ranenko <mranenko@appliedmgt.com>
2024-04-19 09:41:27 -0600

    initial responsive nav functionality

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 interface NavProps {
@@ -5,38 +6,54 @@ interface NavProps {
 }
 
 const Nav = ({ className = '' }: NavProps) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const handleNavToggleClick = () => {
+    setIsActive(!isActive)
+  }
+
   return (
-    <nav className={`nav flex-column gap-1 ${className}`}>
-      <NavLink to='/'>
-        <img
-          alt='Sabor Design System'
-          className='logo'
-          src='/images/logos/sabor-logo-light.svg'
-        />
-      </NavLink>
+    <>
+      <button
+        className={`nav-toggle ${isActive ? 'active' : ''}`}
+        onClick={handleNavToggleClick}
+      >
+        =
+      </button>
+      <nav
+        className={`nav flex-column gap-1 ${className} ${isActive ? 'active' : ''}`}
+      >
+        <NavLink to='/'>
+          <img
+            alt='Sabor Design System'
+            className='logo'
+            src='/images/logos/sabor-logo-light.svg'
+          />
+        </NavLink>
 
-      <div className='flex-column gap-05'>
-        <h3 className='my-05'>Design System</h3>
-        <NavLink to='/'>Overview</NavLink>
-      </div>
+        <div className='flex-column gap-05'>
+          <h3 className='my-05'>Design System</h3>
+          <NavLink to='/'>Overview</NavLink>
+        </div>
 
-      <div className='flex-column gap-05'>
-        <h3 className='my-05'>Foundations</h3>
-        <NavLink to='/foundations/color'>Color</NavLink>
-        <NavLink to='/foundations/typography'>Typography</NavLink>
-        <NavLink to='/foundations/iconography'>Iconography</NavLink>
-        <NavLink to='/foundations/logos'>Logos</NavLink>
-      </div>
+        <div className='flex-column gap-05'>
+          <h3 className='my-05'>Foundations</h3>
+          <NavLink to='/foundations/color'>Color</NavLink>
+          <NavLink to='/foundations/typography'>Typography</NavLink>
+          <NavLink to='/foundations/iconography'>Iconography</NavLink>
+          <NavLink to='/foundations/logos'>Logos</NavLink>
+        </div>
 
-      <div className='flex-column gap-05'>
-        <h3 className='my-05'>Components</h3>
-        <NavLink to='/components/button'>Button</NavLink>
-        <NavLink to='/components/card'>Card</NavLink>
-        <NavLink to='/components/footer'>Footer</NavLink>
-        <NavLink to='/components/header'>Header</NavLink>
-        <NavLink to='/components/input'>Input</NavLink>
-      </div>
-    </nav>
+        <div className='flex-column gap-05'>
+          <h3 className='my-05'>Components</h3>
+          <NavLink to='/components/button'>Button</NavLink>
+          <NavLink to='/components/card'>Card</NavLink>
+          <NavLink to='/components/footer'>Footer</NavLink>
+          <NavLink to='/components/header'>Header</NavLink>
+          <NavLink to='/components/input'>Input</NavLink>
+        </div>
+      </nav>
+    </>
   )
 }
 
