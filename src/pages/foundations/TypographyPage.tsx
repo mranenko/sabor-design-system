fb174a3cd9e12fd52b9a295836ab5b8ac9226cae
Marina Ranenko <mranenko@appliedmgt.com>
2024-04-20 14:28:37 -0600

    typography

--- src/pages/foundations/TypographyPage.tsx
+++ src/pages/foundations/TypographyPage.tsx
@@ -17,15 +17,104 @@ const TypographyPage = () => {
         <Card>
           <h2 className='subtitle'>Font Families</h2>
           <div className='tiles fonts'>
-            <div className='tile font wide'>
-              <span className='sample playfair-display'>AaBbCc</span>
-              <span className='caption'>Playfair Display</span>
-            </div>
-            <div className='tile font wide'>
+            <div className='tile font large'>
               <span className='sample heebo'>AaBbCc</span>
               <span className='caption'>Heebo</span>
             </div>
+            <div className='tile font large'>
+              <span className='sample playfair-display'>AaBbCc</span>
+              <span className='caption'>Playfair Display</span>
+            </div>
           </div>
+        </Card>
+
+        <Card>
+          <h2 className='subtitle'>Text</h2>
+          <table className='table fonts'>
+            <tr>
+              <td>
+                <div className='tile font small'>Text</div>
+              </td>
+              <td className='strong'>text</td>
+              <td className='color-subtle'>
+                font-family-heebo<br />
+                font-weight-semi-bold<br />
+                font-size-regular<br />
+                leading-normal<br />
+                tracking-wide
+              </td>
+              <td className='color-subtle'>
+                'Heebo', sans-serif<br />
+                400 / normal<br />
+                .875rem / 14px<br />
+                1.5 / 150%<br />
+                .5px
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <div className='tile font small strong'>Strong text</div>
+              </td>
+              <td className='strong'>strong-text</td>
+              <td className='color-subtle'>
+                font-family-heebo<br />
+                font-weight-semi-bold<br />
+                font-size-regular<br />
+                leading-normal<br />
+                tracking-wide
+              </td>
+              <td className='color-subtle'>
+                'Heebo', sans-serif<br />
+                600 / semibold<br />
+                .875rem / 14px<br />
+                1.5 / 150%<br />
+                .5px
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <div className='tile font small'>Small text</div>
+              </td>
+              <td className='strong'>small-text</td>
+              <td className='color-subtle'>
+                font-family-heebo<br />
+                font-weight-semi-bold<br />
+                font-size-small<br />
+                leading-normal<br />
+                tracking-wide
+              </td>
+              <td className='color-subtle'>
+                'Heebo', sans-serif<br />
+                400 / normal<br />
+                .75rem / 12px<br />
+                1.5 / 150%<br />
+                .5px
+              </td>
+            </tr>
+            
+            <tr>
+              <td>
+                <div className='tile font small strong'>Small strong text</div>
+              </td>
+              <td className='strong'>small-strong-text</td>
+              <td className='color-subtle'>
+                font-family-heebo<br/>
+                font-weight-semi-bold<br />
+                font-size-small<br />
+                leading-normal<br />
+                tracking-wide
+              </td>
+              <td className='color-subtle'>
+                'Heebo', sans-serif<br />
+                600 / semibold<br />
+                .75rem / 12px<br />
+                1.5 / 150%<br />
+                .5px
+              </td>
+            </tr>
+          </table>
         </Card>
       </section>
     </Page>
